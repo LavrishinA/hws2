4eecd4206f885a0eb55b8166695f256b6ac84b37
AlexeyLavrishin <alexeylavrishin@gmail.com>
2023-11-13 13:32:53 +0300

    feat: hw06 isDone

--- src/s2-homeworks/hw06/HW6.tsx
+++ src/s2-homeworks/hw06/HW6.tsx
@@ -19,7 +19,8 @@ const HW6 = () => {
     }
     const restore = () => {
         // делают студенты
-
+        const stateFromLs = restoreState<string>('hw6-editable-span-value', "edit text...")
+        setValue(stateFromLs)
     }
 
     return (
